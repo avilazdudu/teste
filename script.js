dd1c1c50a7c9a42853dc28c0a2c0a3134a6c8ec9
Eduardo Ávila Pereira <04647306003@senacrs.edu.br>
2024-08-16 11:10:01 -0300

    teste

---
+++ script.js
@@ -0,0 +1,9 @@
+// perguntando ao usuário
+let nome = prompt ("Digite seu nome");
+alert ("Olá "+nome+", tudo bem?")
+// alerta na tela
+alert ("Olá Mundo!");
+// console do navegador
+console.log("Teste de console");
+// inclui um html identificado pelo ID
+document.getElementById("demo").innerHTML= "Eduardo Avila";
